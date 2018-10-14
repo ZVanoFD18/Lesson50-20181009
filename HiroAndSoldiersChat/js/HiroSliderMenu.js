2323f1ee7142324bc3ed3868b903f07d501f1f69
Zaglyada Ivan <ZVanoFD18@gmail.com>
2018-10-14 22:33:12 +0300

    Сделал заготовку HTML+CSS для слайдер-меню "hiro-slider-menu"

---
+++ HiroAndSoldiersChat/js/HiroSliderMenu.js
@@ -0,0 +1,11 @@
+'use strict';
+
+class HiroSliderMenu {
+
+    constructor(options){
+        if (typeof (options) !== 'object'){
+            throw new Error('Не указаны опции.');
+        }
+        //this.items
+    }
+};
